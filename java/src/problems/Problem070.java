879c4e041356996c4a19150d3d0a5f6021176c52
Vineet <vineet@benchling.com>
2013-10-01 23:56:14 -0700

    Fix problem 70 to run 4x faster by terminating early

--- java/src/problems/Problem070.java
+++ java/src/problems/Problem070.java
@@ -17,13 +17,13 @@ public class Problem070 implements Problem {
     private static int solve(int max) {
         double bestRatio = Double.MAX_VALUE;
         int bestNum = 0;
-        System.out.println(System.currentTimeMillis());
         List<Integer> primes = CollectionsUtil.getTrueIndices(MathUtil.primeSieve(max));
-        System.out.println(System.currentTimeMillis());
-        System.out.println(primes.size());
         int limit = (int) Math.sqrt(max) + 1;
-        for (int i = 1; i < primes.size(); i++) {
+        for (int i = primes.size() - 1; i >= 0; i--) {
             if (primes.get(i) > limit) {
+                continue;
+            }
+            if ((1 - 1.0 / primes.get(i)) < 1/bestRatio) {
                 break;
             }
             for (int j = i + 1; j < primes.size(); j++) {
@@ -40,58 +40,7 @@ public class Problem070 implements Problem {
                 }
             }
         }
-        System.out.println(System.currentTimeMillis());
         return bestNum;
-//        for (int numPrimes = 1; numPrimes <= primes.size(); numPrimes++) {
-//            Set<Set<Integer>> combinations = CollectionsUtil.combinations(
-//                    primes, numPrimes);
-//            System.out.println(numPrimes);
-//            for (Set<Integer> set : combinations) {
-//                int n = 1;
-//                for (int num : set) {
-//                    n *= num;
-//                }
-//                int phi = n;
-//                for (int num : set) {
-//                    phi -= phi / num;
-//                }
-//                double ratio = n * 1.0 / phi;
-//                if (ratio < bestRatio) {
-//                    char[] arr1 = Integer.toString(n).toCharArray();
-//                    char[] arr2 = Integer.toString(phi).toCharArray();
-//                    Arrays.sort(arr1);
-//                    Arrays.sort(arr2);
-//                    if (Arrays.equals(arr1, arr2)) {
-//                        bestRatio = ratio;
-//                        bestNum = n;
-//                    }
-//                }
-//            }
-//            System.out.println(numPrimes);
-//            if (bestNum != 0) {
-//                return bestNum;
-//            }
-//        }
-//        // for (int i = 2; i <= max; i++) {
-//        // Set<Integer> divisors = MathUtil.primeFactors(i);
-//        // int phi = i;
-//        // for (int num : divisors) {
-//        // phi -= phi/num;
-//        // }
-//        // if (i*1.0/phi < bestRatio) {
-//        // char[] arr1 = Integer.toString(i).toCharArray();
-//        // char[] arr2 = Integer.toString(phi).toCharArray();
-//        // Arrays.sort(arr1);
-//        // Arrays.sort(arr2);
-//        // if (Arrays.equals(arr1, arr2)) {
-//        // // System.out.println(i + " " + phi);
-//        // bestRatio = i*1.0/phi;
-//        // bestNum = i;
-//        // }
-//        // }
-//        // // System.out.println(i + ": " + phi);
-//        // }
-//        return bestNum;
     }
 
     private static boolean isValid(int n, int phi) {
@@ -101,14 +50,4 @@ public class Problem070 implements Problem {
         Arrays.sort(arr2);
         return Arrays.equals(arr1, arr2);
     }
-
-    private static int phi(int n) {
-        Set<Integer> divisors = MathUtil.primeFactors(n);
-        int phi = n;
-        for (int num : divisors) {
-            phi -= phi / num;
-        }
-        return phi;
-    }
-
 }
